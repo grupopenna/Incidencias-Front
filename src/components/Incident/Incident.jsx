27611249e3f8f4eda3011159537537cbce444bf6
carogs6 <sistemas6@compc.com.ar>
2023-10-24 16:53:16 -0300

    mati nos hecho

--- src/components/Incident/Incident.jsx
+++ src/components/Incident/Incident.jsx
@@ -1,20 +1,33 @@
-const Incident = (prop) => {
+/* eslint-disable react/prop-types */
+/* eslint-disable no-prototype-builtins */
+
+
+const Incident = ({item}) => {
+  console.log('item.fields', item.fields)
+  console.log('item.fields.responsable', item.fields.assignee?.displayName == null)
+
   return (
-    <div className="h-fit mb-10 flex flex-col">
-      <div className="h-fit mb-4">
-        <h2 className="font-bold text-2xl">{prop.title}</h2>
-      </div>
-      {/* <img src={prop.img} alt="" className="h-64 "/> */}
-      <p>{prop.description}</p>
-      {/* <p>{prop.state}</p> */}
-      {/* <p>{prop.coments}</p> */}
+    <div className="h-full w-full rounded-md bg-bgIncident mb-3 flex flex-col p-1 text-font">
+        {/* key={item.fields.fields.key}
+                      id={item.fields.id}
+                      img={item.fields.image}
+                      title={item.fields.fields.summary}
+                      description={item.fields.fields.description}
+                      state={item.fields.fields.status.name}
+                      responsable={item.fields.fields?.assignee}
+                      hsConsumidas={item.fields.fields.timetracking.timeSpent}
+                      hsEstimadas={item.fields.fields.timetracking.remainingEstimate} */}
+      <h2 className="text-font font-bold text-lg">{item.fields.summary}</h2>
+      <p className="text-base">{item.key}</p>
+      {/* <img src={item.fields.img} alt="" className="h-64 "/> */}
+      <p>{item.fields.timetracking.timeSpent}</p>
+      {/* <p>{item.fields.coments}</p> */}
       <div>
-        {/* {prop.responsable && prop.responsable} */}
+        <p>Responsable {item.fields.assignee?.displayName}</p>
       </div>
-      {/* <p>Tiempo invertido: {prop.hsConsumidas}</p>
-      <p>Tiempo estimado: {prop.hsEstimadas}</p>
-      <p>{prop.process}</p>
-      <p>{prop.priority}</p> */}
+      {/* {item.fields.timeSpent == null ? "" : <p> {item.fields.timeSpent }</p> } */}
+      {/* <p>{item.fields.process}</p>
+      <p>{item.fields.priority}</p> */}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "#222933",
+        background: "#1D2125",
         componentBg: "#374151",
         buttonBg: "#3B82F6",
-        font: "#FFFFFF",
+        font: "#8C9BAB",
         fontPlaceholder: "#929DAE",
         inoutRing: "#6789B9",
-        hoverButton: "#1D4ED8"
+        hoverButton: "#1D4ED8",
+        bgColumn: "#161A1D",
+        bgIncident: "#22272B"
       },
       titilliumWeb: "'Titillium Web', 'sans-serif'",
       roboto: "'Roboto', 'sans-serif';"
