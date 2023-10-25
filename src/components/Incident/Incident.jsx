c5c3d2d2ee5fea97404df0206ec10d0eef003d2e
carogs6 <sistemas6@compc.com.ar>
2023-10-25 11:59:35 -0300

    integrated incident

--- src/components/Incident/Incident.jsx
+++ src/components/Incident/Incident.jsx
@@ -5,33 +5,21 @@ import { WithoutPhoto } from "../Icon";
 
 
 const Incident = ({ item }) => {
-  //console.log('item.fields.responsable', item.fields.assignee?.displayName == null)
 
   return (
     <div className="h-40 w-64 rounded-md bg-bgIncident mb-3 flex justify-end items-center flex-col p-1 text-font">
-      {/* key={item.fields.fields.key}
-                      id={item.fields.id}
-                      img={item.fields.image}
-                      title={item.fields.fields.summary}
-                      description={item.fields.fields.description}
-                      state={item.fields.fields.status.name}
-                      responsable={item.fields.fields?.assignee}
-                      hsConsumidas={item.fields.fields.timetracking.timeSpent}
-                      hsEstimadas={item.fields.fields.timetracking.remainingEstimate} */}
       <div className="w-full">
 
       <p className="text-font font-bold text-lg">{item.fields.summary}</p>
       </div>
-      {/* <img src={item.fields.img} alt="" className="h-64 "/> */}
       <p>{item.fields.timetracking.timeSpent}</p>
-      {/* <p>{item.fields.coments}</p> */}
       <div className="w-full h-1/2 items-end justify-between p-2 flex">
-       <div className="flex items-center gap-2">
-         <img src={item?.fields.issuetype.iconUrl} alt="Imagen del icono del proyecto de jira" className="w-4 h-4" />
-         <p className="text-base">{item.key}</p>
-       </div>
-       { item?.fields?.assignee?.avatarUrls ?
-       <img 
+        <div className="flex items-center gap-2">
+          <img src={item?.fields.issuetype.iconUrl} alt="Imagen del icono del proyecto de jira" className="w-4 h-4" />
+          <p className="text-base">{item.key}</p>
+        </div>
+        { item?.fields?.assignee?.avatarUrls ?
+        <img 
         className="w-6 h-6" 
         src={item?.fields?.assignee?.avatarUrls['16x16']} 
         alt={`persona asignada ${item?.fields?.assignee?.displayName}`} 
@@ -40,9 +28,6 @@ const Incident = ({ item }) => {
       }
 
       </div>
-      {/* {item.fields.timeSpent == null ? "" : <p> {item.fields.timeSpent }</p> } */}
-      {/* <p>{item.fields.process}</p>
-      <p>{item.fields.priority}</p> */}
     </div>
   )
 }
--- src/components/Tablero/Tablero.jsx
+++ src/components/Tablero/Tablero.jsx
@@ -1,31 +1,20 @@
 /* eslint-disable react/no-unknown-property */
 import { useSelector } from "react-redux";
-// import Incident from "../Incident/Incident";
 import { useEffect, useState } from "react";
-// import { postTransition } from "../../redux/actions/transitions/postTransition";
 import { getIssue } from "../../redux/actions/issue/getIssue";
-// import { useLocation, useNavigate } from "react-router-dom";
 import Modal from "../Modal/Modal";
 import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd"; 
+import Incident from "../Incident/Incident";
 
 const Tablero = () => {
   
-  // const [ listPriorizado, setListPriorizado] = useState(getList("Priorizado"))
-  // const navigate = useNavigate();
-  //const dispatch = useDispatch();
   const [modalShow, setModalShow] = useState(false)
   const [itemSelect, setItemSelect] = useState({})
   const incidents = useSelector((state) => state.incients);
   const transitions = useSelector((state) => state.transitions);
-  // const [incident, setIncident] = useState(incidents);
   const [reload, setReload] = useState(false);
-  // const navigate = useNavigate();
 
   console.log('incidents', incidents)
-
-  // const location = useLocation()
-  // const { pathname } = location;
-  // const keyPathname = pathname.split('/').slice(-1)
 
   useEffect(() => {
     getIssue()
@@ -43,14 +32,11 @@ const Tablero = () => {
     return filterList
   }
 
-  // const [ listSinPriorizar, setListSinPriorizar] = useState(getList("Sin Priorizar"))
-  // console.log('listSinPriorizar', listSinPriorizar)
   const [state, setState] = useState([ getList("Priorizado"), getList("En Proceso"), getList("Validar"), getList("Validado") ]);
 
   function onDragEnd(result) {
     const { source, destination } = result;
 
-    // dropped outside the list
     if (!destination) {
       return;
     }
@@ -80,9 +66,6 @@ const Tablero = () => {
     return result;
   };
 
-  /**
-   * Moves an item from one list to another list.
-   */
   const move = (source, destination, droppableSource, droppableDestination) => {
     const sourceClone = Array.from(source);
     const destClone = Array.from(destination);
@@ -99,15 +82,12 @@ const Tablero = () => {
   const grid = 8;
 
   const getItemStyle = (isDragging, draggableStyle) => ({
-    // some basic styles to make the items look a bit nicer
     userSelect: "none",
     padding: grid * 2,
     margin: `0 0 ${grid}px 0`,
 
-    // change background colour if dragging
     background: isDragging ? "lightgreen" : "grey",
 
-    // styles we need to apply on draggables
     ...draggableStyle
   });
   const getListStyle = isDraggingOver => ({
@@ -115,85 +95,11 @@ const Tablero = () => {
     padding: grid,
     width: 250
   });
-  // const startDrag = (evt, item) => {
-  //   evt.dataTransfer.setData('itemID', item.id)
-  // }
-
-  // const draggingOver = (evt) => {
-  //   evt.preventDefault();
-  // }
-
-  // const onDrop = (evt, list) => {
-  //   const itemID = evt.dataTransfer.getData('itemID');
-  //   const item = incidents.find(item => item.id == itemID);
-
-  //   const data = {
-  //     id: item.id,
-  //     list: list.to.name
-  //   }
-
-  //   postTransition(data)(dispatch).then((response) => {
-  //     console.log('response', response)
-  //   }).catch((error) => console.log('response tablero L38', error))
-  //   item.list = list;
-
-  //   const newState = incident.map(task => {
-  //     if (task.id === itemID) return item;
-  //     return task
-  //   })
-
-  //   setIncident(newState);
-  // }
-
-  // const handleNotify = () => {
-  //   const issueId = incidents[0].fields.issuetype.id
-  //   navigate(`/createIssue/form/${keyPathname[0]}/${issueId}`)
-  // }
-
-  // const handleReload = () => {
-  //   setReload(true)
-  // }
-
 
   return (
-    // <div className="flex justify-center flex-col w-full mx-4">
-    //   {modalShow && <Modal setModalShow={setModalShow} itemSelect={itemSelect} />}
-    //   <div className="flex justify-around my-5">
-    //     <button onClick={() => { handleNotify() }} className="bg-buttonBg w-44 h-10 rounded-md">Notificar Incidencias</button>
-    //     <button onClick={() => { handleReload() }} className="">
-    //       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="text-slate-100 w-6 h-6 bg-buttonBg p-3">
-    //         <path d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-    //       </svg>
-    //     </button>
-    //   </div>
-    //   <div className="flex gap-x-3 w-full">
-    //     {
-    //       transitions.map((transition) => <div key={transition.id} className="bg-bgColumn rounded-2xl pt-5 min-h-full w-5/6">
-    //         <h1 className="mx-2 mb-1 text-font">{transition.to.name}</h1>
-    //         {getList(transition.to.name).map((item) => (
-
-    //           <div key={item.id} className="flex justify-center mx-2 ">
-    //             <button onClick={() => { setModalShow(true), setItemSelect(item) }}>
-    //               <Incident
-    //                 item={item}
-    //               />
-    //             </button>
-    //           </div>
-    //         ))}
-    //       </div>)
-    //     }
-    //   </div>
-    // </div>
     <div>
       {modalShow && <Modal setModalShow={setModalShow} itemSelect={itemSelect} />}
-      {/* <div>
-        <Droppable  droppableId={`SinPriorizar`}>
-          {listSinPriorizar.map((item) => 
-          <h1 key={item.key}>{item.id}</h1>
-          )}
-        </Droppable>
-        
-      </div> */}
+
       <div className="flex gap-x-5">
         <DragDropContext onDragEnd={onDragEnd}>
           {transitions.map((transition) => (
@@ -202,14 +108,6 @@ const Tablero = () => {
                 <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)} {...provided.droppableProps}>
                   <h1 className="mx-2 mb-1 text-font">{transition.to.name}</h1>
                   {getList(transition.to.name).map((item, index) => (
-                    //   <div key={item.id} className="flex justify-center mx-2 ">
-                    //     <button onClick={() => { setModalShow(true), setItemSelect(item) }}>
-                    //       <Incident
-                    //         item={item}
-                    //       />
-                    //     </button>
-                    //   </div>
-                    // ))}git
                     <button key={item.id} onClick={() => { setModalShow(true), setItemSelect(item) }} className="w-full">
                       <Draggable key={item.id} draggableId={item.id} index={index}>
                           {(provided, snapshot) => (
@@ -221,9 +119,9 @@ const Tablero = () => {
                                   )}
                               >
                                   <div className="flex flex-col w-full">
-                                    <p>{item.fields.summary}
-                                    </p>
-                                    <p>{item.key}</p>
+                                    {<Incident item={item}/>}
+                                    {/* <p>{item.fields.summary}</p>
+                                    <p>{item.key}</p> */}
                                   </div>
                               </div>
                           )}
