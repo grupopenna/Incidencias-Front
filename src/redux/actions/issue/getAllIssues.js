4d3088229f89a0790357c9049bbf92486ce0f621
carogs6 <sistemas6@compc.com.ar>
2023-10-27 11:21:14 -0300

    Fix response data error in modal and in all incidents

--- src/redux/actions/issue/getAllIssues.js
+++ src/redux/actions/issue/getAllIssues.js
@@ -3,19 +3,44 @@ import {  BASE_URL, GET_All_ISSUES } from '../../action-type';
 
 export const getAllIssues = () => {
 
-  let userId = "712020:75da847b-f656-4020-a3fd-84d8811cd76f"
+  let userId = "712020:8a4ac3e0-8800-405a-96a0-a09c82e1a727"
 
   const bodyData = {
+    "expand": [
+      "names"
+      // "operations"
+    ],
+    "fields": [
+        "description",
+        "issuetype",
+        "summary",
+        "status",
+        "assignee",
+        "accountId",
+        "timetracking",
+        "timeoriginalestimate",
+        "aggregatetimeestimate",
+        "aggregatetimespent",
+        "customfield_10019",
+        "worklog",
+        "attachment",
+        "project",
+        "created",
+        "updated"
+    ],
     "jql": `reporter=${userId} order by created DESC`
   }
 
   return async (dispatch) => {
     try {
       const response = (await axios.post(`${BASE_URL}/incident/searchAll`, bodyData)).data;
+      console.log('response', response)
 
-      dispatch({type: GET_All_ISSUES, payload: response})
+      const filteredData = response?.filter((item) => item?.fields.project.projectCategory.name === 'notificacionesIncidencias')
 
-      return response
+      dispatch({type: GET_All_ISSUES, payload: filteredData})
+
+      return filteredData
     } catch (error) {
       console.log('Error al realizar la solicitud getAllIssues');
 
--- src/redux/actions/issue/getIssue.js
+++ src/redux/actions/issue/getIssue.js
@@ -3,11 +3,37 @@ import {  BASE_URL, GET_ISSUES } from '../../action-type';
 
 export const getIssue = (key) => {
 
-  let userId = "712020:75da847b-f656-4020-a3fd-84d8811cd76f"
+  const idUser = "712020:75da847b-f656-4020-a3fd-84d8811cd76f"
+
+  let bodyData = {
+    "expand": [
+        "names",
+        // "schema",
+        "operations"
+    ],
+    "fields": [
+      "description",
+      "issuetype",
+      "summary",
+      "status",
+      "assignee",
+      "accountId",
+      "timetracking",
+      "timeoriginalestimate",
+      "aggregatetimeestimate",
+      "aggregatetimespent",
+      "customfield_10019",
+      "worklog",
+      "attachment"
+    ],
+    "jql": `project = ${key} and reporter=${idUser}`
+}
 
   return async (dispatch) => {
     try {
-      const response = (await axios.get(`${BASE_URL}/incident/search/${key}/${userId}`)).data;
+      const response = (await axios.post(`${BASE_URL}/incident/search/`, bodyData)).data;
+
+      console.log('response', response)
 
       dispatch({type: GET_ISSUES, payload: response})
 
