31c243f0cab6c830e57c37cd3c3963b0cc3d3ad3
SistemasComPc8 <sistemas8@compc.com.ar>
2023-10-27 16:54:17 -0300

    notificacion al usuario y tablero de sprint

--- src/components/Tablero/Tablero.jsx
+++ src/components/Tablero/Tablero.jsx
@@ -127,7 +127,7 @@ const Tablero = () => {
               <path d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
           </svg>
         </button> */}
-        {keyPathname[0] === 'CMS' || keyPathname[0] === 'CFS' &&
+        {keyPathname[0] === 'CMS' &&
           <div className="flex items-center rounded-lg pl-2 bg-bgIncident">
             <AlertIcon />
             <span className="text-white">Si su tarjeta no esta revise los mail ó</span>
