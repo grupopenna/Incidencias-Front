579714620046355d8f7fbfc3e45ba43726c3190d
SistemasComPc8 <sistemas8@compc.com.ar>
2023-10-31 10:52:38 -0300

    bugs

--- src/components/NotifyIncidentForm/NotifyIncidentForm.jsx
+++ src/components/NotifyIncidentForm/NotifyIncidentForm.jsx
@@ -2,28 +2,26 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 import { issuePost, getIssueTypes } from "../../redux/actions/";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useRef } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import {  Select, SelectItem  } from '@tremor/react'
+import { Select, SelectItem } from '@tremor/react'
 import { Editor as TuiEditor } from '../Editor/index'
 
-const NotifyIncidentForm = (key) => {
+const NotifyIncidentForm = () => {
+
   const editorRef = useRef(null)
   const dispatch = useDispatch()
   const { issuesType, id } = useSelector(state => state.issuesTypes)
-
-  // const [content, setContent] = useState()
-
-
   const [titleDesc, setTitleDesc] = useState('');
   const [email, setEmail] = useState('');
   const [selectedIssue, setSelectedIssue] = useState('')
   const [errors, setErrors] = useState({ titleDesc: '', email: '', descripcion: '' });
   const location = useLocation()
+  const navigate = useNavigate();
   const { pathname } = location;
-  // Toolbar edito
-  //console.log(content)
-  
+  const [IssueKey] = pathname.split('/').slice(-2)
+
+
   useEffect(() => {
 
     (async () => {
@@ -35,7 +33,7 @@ const NotifyIncidentForm = (key) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const descripcion = editorRef.current.getMarkdown() 
+    const descripcion = editorRef.current.getMarkdown()
 
     // Validación de nombre no vacío
     if (!titleDesc.trim()) {
@@ -58,7 +56,7 @@ const NotifyIncidentForm = (key) => {
 
     // Restablece los mensajes de error en caso de éxito
     setErrors({ titleDesc: '', email: '', descripcion: '' });
-    const data = { key, titleDesc, email, descripcion, projectId: id, issueId: selectedIssue }
+    const data = { IssueKey, titleDesc, email, descripcion, projectId: id, issueId: selectedIssue }
     // Ahora, realiza la solicitud POST usando axios
     //--------------------------------------------------------
     /*  await issuePost(data).then((response)=>{
@@ -76,102 +74,107 @@ const NotifyIncidentForm = (key) => {
 
 
   return (
-    <div className=' flex justify-center mx-3 lg:px-16'>
-      <div className='flex flex-col w-full lg:w-2/4'>
-        <form onSubmit={handleSubmit}>
-          {/* <div className="text-red-500">
+    <>
+      <div className='ml-7 mt-5'>
+        <button onClick={() => navigate(-1)} className='bg-buttonBg px-3 py-2 text-white rounded-lg'>Atras</button>
+      </div>
+      <div className='flex justify-center mx-3 lg:px-16'>
+        <div className='flex flex-col w-full lg:w-2/4'>
+          <form onSubmit={handleSubmit}>
+            {/* <div className="text-red-500">
             {errors.usuario && <div>{errors.usuario}</div>}
             {errors.email && <div>{errors.email}</div>}
             {errors.descripcion && <div>{errors.descripcion}</div>}
           </div> */}
-          <div className="space-y-12">
-            <div className="pb-12">
-              <div className="mt-8 grid grid-cols-1 gap-x-2 gap-y-4">
-                <div className="">
-                  <label htmlFor="titleDesc" className="block text-sm font-medium leading-6 text-slate-100">
-                    Titulo Descriptivo*
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      type="text"
-                      name="title"
-                      id="titleDesc"
-                      autoComplete="given-name"
-                      className="px-3 block w-full rounded-md border-0 py-1.5 text-slate-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-fontPlaceholder focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      value={titleDesc}
-                      onChange={(e) => setTitleDesc(e.target.value)}
-                    />
+            <div className="space-y-12">
+              <div className="pb-12">
+                <div className="mt-8 grid grid-cols-1 gap-x-2 gap-y-4">
+                  <div className="">
+                    <label htmlFor="titleDesc" className="block text-sm font-medium leading-6 text-slate-100">
+                      Titulo Descriptivo*
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        type="text"
+                        name="title"
+                        id="titleDesc"
+                        autoComplete="given-name"
+                        className="px-3 block w-full rounded-md border-0 py-1.5 text-slate-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-fontPlaceholder focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        value={titleDesc}
+                        onChange={(e) => setTitleDesc(e.target.value)}
+                      />
+                    </div>
                   </div>
-                </div>
-                <div className="">
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-slate-100">
-                    Email*
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      id="email"
-                      name="email"
-                      // type="email"
-                      autoComplete="email"
-                      className="px-3 block w-full rounded-md border-0 py-1.5 text-slate-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-fontPlaceholder focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
+                  <div className="">
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-slate-100">
+                      Email*
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="email"
+                        name="email"
+                        // type="email"
+                        autoComplete="email"
+                        className="px-3 block w-full rounded-md border-0 py-1.5 text-slate-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-fontPlaceholder focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
                   </div>
-                </div>
-                <label className='text-white'>
-                  Issue type
-                  <Select 
-                    onValueChange={(value) => setSelectedIssue(value)}
-                    className='z-50 mt-2'
-                    value={selectedIssue}>
-                  {issuesType?.map((project) => (
-                      <SelectItem key={project.id} value={project.id}>
+                  <label className='text-white'>
+                    Issue type
+                    <Select
+                      onValueChange={(value) => setSelectedIssue(value)}
+                      className='z-50 mt-2'
+                      value={selectedIssue}>
+                      {issuesType?.map((project) => (
+                        <SelectItem key={project.id} value={project.id}>
                           {project.name}
-                      </SelectItem>
+                        </SelectItem>
                       ))}
-                  </Select>
-                </label>
-                <div className='mt-1'>
-                  <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-slate-100">
-                    Detalle de Incidencia*
+                    </Select>
                   </label>
-                  <div className="mt-1">
-                    <TuiEditor markdownRef={editorRef}/>
+                  <div className='mt-1'>
+                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-slate-100">
+                      Detalle de Incidencia*
+                    </label>
+                    <div className="mt-1">
+                      <TuiEditor markdownRef={editorRef} />
+                    </div>
                   </div>
-                </div>
-                <div className="">
-                  <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-slate-100">
-                    Subir archivo
-                  </label>
-                  <div className="mt-1 flex rounded-lg border border-dashed border-gray-300/25 p-1">
-                    <div className="text-center">
-                      <div className="flex text-sm leading-6 text-gray-400">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative px-2 cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 hover:bg-sky-200"
-                        >
-                          <span>Subir archivo</span>
-                          <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                        </label>
+                  <div className="">
+                    <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-slate-100">
+                      Subir archivo
+                    </label>
+                    <div className="mt-1 flex rounded-lg border border-dashed border-gray-300/25 p-1">
+                      <div className="text-center">
+                        <div className="flex text-sm leading-6 text-gray-400">
+                          <label
+                            htmlFor="file-upload"
+                            className="relative px-2 cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 hover:bg-sky-200"
+                          >
+                            <span>Subir archivo</span>
+                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                          </label>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="mt-9 flex items-center justify-center">
-                <button
-                  type="submit"
-                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Informar incidencia
-                </button>
+                <div className="mt-9 flex items-center justify-center">
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Informar incidencia
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
