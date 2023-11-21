04630e9319a342c04debf8b44a70cff71321ce2e
lucianoSistemas7 <sistemas7@compc.com.ar>
2023-11-21 15:33:05 -0300

    fix: send an array instead of a string

--- src/redux/actions/issue/getIssue.js
+++ src/redux/actions/issue/getIssue.js
@@ -4,6 +4,8 @@ import { BASE_URL, GET_ISSUES } from '../../action-type';
 export const getIssue = (key, userId) => {
 
   console.log({ key })
+  if (key?.trim() === '') return 
+  
   return async (dispatch) => {
     try {
       const response = (await axios.get(`${BASE_URL}/incident/search/${key}/${userId}`));
