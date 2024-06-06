62617d45fc103dda53832673176a10f345879269
carogs6 <146481985+carogs6@users.noreply.github.com>
2024-06-06 15:19:18 -0300

    Update getIssueType.js

--- src/redux/actions/issue/getIssueType.js
+++ src/redux/actions/issue/getIssueType.js
@@ -3,12 +3,14 @@ import {  BASE_URL, GET_ISSUETYPE } from '../../action-type';
 
 const ISSUE_TYPES_USER = {
     TAREA: 'Tarea',
-    ERROR: 'Error'
+    TRABAGESTION: 'Traba gestion',
+    NR: 'NuevoRequerimiento'
 }
 
 const SHOW_ISSUE_TYPES = {
-    [ISSUE_TYPES_USER.ERROR]: true,
-    [ISSUE_TYPES_USER.TAREA]: true
+    [ISSUE_TYPES_USER.TRABAGESTION]: true,
+    [ISSUE_TYPES_USER.TAREA]: true,
+    [ISSUE_TYPES_USER.NR]: true,
 }
 
 export const getIssueTypes = (projectId) => {
@@ -17,6 +19,7 @@ export const getIssueTypes = (projectId) => {
     return async (dispatch) => {
         try {
             const response = await axios(`${BASE_URL}/board/getIssuestype/${projectId}`)
+            console.log(response);
             const filteredData = response.data?.issuesType?.filter((issue) => SHOW_ISSUE_TYPES[issue.name] )
             dispatch({ type: GET_ISSUETYPE, payload: {issuesType: filteredData, id: response.data.id}})
         } catch (err) {
