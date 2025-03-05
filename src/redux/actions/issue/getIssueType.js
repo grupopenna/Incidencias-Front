dab453a708280d53fe7d4a96b04b9954aa0bb48a
SistemasComPc8 <146384065+SistemasComPc8@users.noreply.github.com>
2025-03-05 08:33:45 -0300

    Update getIssueType.bloqueo-type

--- src/redux/actions/issue/getIssueType.js
+++ src/redux/actions/issue/getIssueType.js
@@ -13,8 +13,8 @@ const SHOW_ISSUE_TYPES = {
     [ISSUE_TYPES_USER.TRABAGESTION]: true,
     [ISSUE_TYPES_USER.TAREA]: true,
     [ISSUE_TYPES_USER.NR]: true,
-    [ISSUE_TYPES_USER.CRITICO]: true,
-    [ISSUE_TYPES_USER.RESOLUCION24]: true
+    // [ISSUE_TYPES_USER.CRITICO]: true,
+    // [ISSUE_TYPES_USER.RESOLUCION24]: true
 }
 
 export const getIssueTypes = (projectId) => {
