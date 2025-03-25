58a63bdf0a3941166347bd0a106e273aa6335120
SistemasComPc8 <sistemas8@compc.com.ar>
2025-03-25 12:29:14 -0300

    issue type DEBO

--- src/redux/actions/issue/getIssueType.js
+++ src/redux/actions/issue/getIssueType.js
@@ -17,14 +17,18 @@ const SHOW_ISSUE_TYPES = {
     [ISSUE_TYPES_USER.RESOLUCION24]: true
 }
 
+
+const SHOW_ISSUE_TYPES_DEBO = {
+    [ISSUE_TYPES_USER.TRABAGESTION]: true,
+    [ISSUE_TYPES_USER.TAREA]: true, 
+}
+
 export const getIssueTypes = (projectId) => {
-
-
     return async (dispatch) => {
         try {
             const response = await axios(`${BASE_URL}/board/getIssuestype/${projectId}`)
-            console.log(response);
-            const filteredData = response.data?.issuesType?.filter((issue) => SHOW_ISSUE_TYPES[issue.name] )
+            //console.log('RES_ISSUETYPE', response.data.issuesType)
+            const filteredData = response.data?.issuesType?.filter((issue) => projectId === 'DEBO' ? SHOW_ISSUE_TYPES_DEBO[issue.name] : SHOW_ISSUE_TYPES[issue.name] )
             dispatch({ type: GET_ISSUETYPE, payload: {issuesType: filteredData, id: response.data.id}})
         } catch (err) {
             console.log(err)
